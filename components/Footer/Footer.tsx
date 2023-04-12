fd52aac66ce11cdccd35440c7081eb7b8e2d46de
AlexandrParhomenko <sania_38@list.ru>
2023-04-12 12:10:40 +0600

    init: start project

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,6 +1,20 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 
 const Footer: FC = () => {
+useEffect(() => {
+  fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films/301', {
+    method: 'GET',
+    headers: {
+      'X-API-KEY': '9867f920-35d0-4b28-b36a-2f5e7d5100f1',
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
+}, [])
+
+
   return <footer>Footer</footer>
 }
 
