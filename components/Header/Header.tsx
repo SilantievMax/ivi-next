5b7e712776e7db73261b25e1cc35b97fb1087e86
SilantievMax <nohrin.max@yandex.ru>
2023-04-16 13:07:51 +0300

    added functional popup in  Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,34 +1,62 @@
 import Link from 'next/link'
-import React, { FC } from 'react'
+import React, { FC, useRef, useState } from 'react'
 
 import { Button } from 'components/Button/Button'
+import ModalWindow from 'components/ModalWindow/ModalWindow'
 
 import { bell, logo, magnifyingGlass, paple } from 'img/imges'
 
 import styles from './Header.module.scss'
 
 const Header: FC = () => {
+  const [isHovering, setIsHovering] = useState(false)
+  const refContainer = document.body
+  console.log(refContainer)
+  const handleMouseOver = () => {
+    setIsHovering(true)
+  }
+
+  const handleMouseOut = () => {
+    setIsHovering(false)
+  }
   return (
     <header className={styles.header}>
       <div className={styles.header__t}>
-        <Link href='/'>
+        <Link href='/' onMouseOver={handleMouseOut}>
           <img src={logo.src} alt='logo' />
         </Link>
         <nav className={styles.nav}>
-          <Link href='/'>Мой Иви</Link>
-          <Link href='/new'>Что нового</Link>
-          <Link href='/movies'>Фильмы</Link>
-          <Link href='/series'>Сериалы</Link>
-          <Link href='/animation'>Мультфильмы</Link>
-          <Link href='/tvplus'>TV+</Link>
+          <Link href='/' onMouseOver={handleMouseOut}>
+            Мой Иви
+          </Link>
+          <Link href='/new' onMouseOver={handleMouseOut}>
+            Что нового
+          </Link>
+          <Link href='/movies' onMouseOver={handleMouseOver}>
+            Фильмы
+          </Link>
+          <Link href='/series' onMouseOver={handleMouseOver}>
+            Сериалы
+          </Link>
+          <Link href='/animation' onMouseOver={handleMouseOver}>
+            Мультфильмы
+          </Link>
+          <Link href='/tvplus' onMouseOver={handleMouseOver}>
+            TV+
+          </Link>
         </nav>
       </div>
       <div className={styles.header__b}>
-        <Button color="gradient" children='Оплатить подписку' />
-        <Button img={magnifyingGlass.src} children={<h3 className={styles.color}>Поиск</h3>} />
-        <Button img={bell.src} />
-        <Button  className={styles.costom_btn} img={paple.src} />
+        <Button color='gradient' children='Оплатить подписку' onMouseOver={handleMouseOut} />
+        <Button img={magnifyingGlass.src} children={<h3 className={styles.color}>Поиск</h3>} onMouseOver={handleMouseOut} />
+        <Button img={bell.src} onMouseOver={handleMouseOver} />
+        <Button className={styles.costom_btn} img={paple.src} onMouseOver={handleMouseOver} />
       </div>
+      {isHovering && (
+        <div onMouseLeave={handleMouseOut}>
+          <ModalWindow />
+        </div>
+      )}
     </header>
   )
 }
---
+++ components/ModalWindow/ModalWindow.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react'
+
+import styles from './ModalWindow.module.scss'
+
+const ModalWindow: FC = () => {
+  return (
+    <div className={styles.sectin}>
+      <div className={styles.line}></div>
+      ModalWindow
+    </div>
+  )
+}
+
+export default ModalWindow
