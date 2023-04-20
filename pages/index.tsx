c73171b7a6c9c9430936d1819cd68323c0bdcc24
Aleksei Anufriev <axoft10@gmail.com>
2023-04-20 08:27:12 +0700

    server request test

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,13 @@ import React, { FC } from 'react';
 
 
 import HomePage from '@/src/components/screens/HomePage/HomePage';
+import PersonPage from '@/src/components/screens/PersonPage/PersonPage';
 
 
 const Home: FC = () => {
   return (
     <>
-      <HomePage />
+      <PersonPage />
     </>
   )
 }
