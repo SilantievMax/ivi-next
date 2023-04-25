da6cb6b1651452b7b0349226737f9607d9580136
Aleksei Anufriev <axoft10@gmail.com>
2023-04-25 15:37:49 +0700

    bug fix

--- src/i18next/i18n.ts
+++ src/i18next/i18n.ts
@@ -1,5 +1,4 @@
 import i18n from 'i18next'
-// import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
 
 import ru from './locales/ru/translation.json';
@@ -7,7 +6,6 @@ import en from './locales/en/translation.json';
 
 i18n
   .use(initReactI18next)
-  // .use(LanguageDetector)
   .init({
     debug: true,
     fallbackLng: 'ru',
