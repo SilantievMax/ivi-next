8acb05a7caaf5223f3719c14b45af10852dd391d
SilantievMax <nohrin.max@yandex.ru>
2023-04-26 17:05:43 +0300

    get AuthSlice

--- src/store/reducers/authReducer.ts
+++ src/store/reducers/authReducer.ts
@@ -1,3 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit'
+
 export const selectIsAuth = () => false
 
-// export const selectIsAuth = state => Boolean(state.auth.data)
+export interface IAuth {
+  ipOpenAuth: boolean
+}
+
+const initialState: IAuth = {
+  ipOpenAuth: false
+}
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setOpenAuth: (state, action) => {
+      state.ipOpenAuth = action.payload
+    }
+  }
+})
+
+// export const authReducer = authSlice.actions
+export const { setOpenAuth } = authSlice.actions
