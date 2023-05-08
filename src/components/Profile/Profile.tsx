47427708402dcee8e06df6e79d27cc1e35ecaccb
SilantievMax <nohrin.max@yandex.ru>
2023-05-08 17:33:28 +0300

    modified feature

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link'
 import React, { FC } from 'react'
 import { useDispatch } from 'react-redux'
 
+import LanguageBtn from '../LanguageBtn/LanguageBtn'
+
 import styles from './Profile.module.scss'
 import { Button } from '@/src/components/Button/Button'
 import { bookmark, card, clipboard, rectangleStack, share, trophy, tvCategory, union } from '@/src/img/imges'
@@ -35,11 +37,16 @@ const Profile: FC = () => {
       </div>
       <div className={styles.card_r}>
         <Button onClick={() => dispatch(setOpenAuth(true))} size='border' children='Войти или зарегистрироваться' color='redOpacity' />
-        <div className={styles.settings}>
-          <Link href='/profile/settings'>Настройки</Link>
-          <a href='https://ask.ivi.ru/?_gl=1*1msjass*_ga*NjYzNjY3NDguMTY1MTQyOTE1MA..*_ga_GETQ4387MJ*MTY4MTYyNTQ5NS4yOS4xLjE2ODE2Mjg4NjMuNDYuMC4w' target='_blank'>
-            Помощь
-          </a>
+        <div className={styles.settings_wrapper}>
+          <div className={styles.settings}>
+            <Link href='/profile/settings'>Настройки</Link>
+            <a href='https://ask.ivi.ru/?_gl=1*1msjass*_ga*NjYzNjY3NDguMTY1MTQyOTE1MA..*_ga_GETQ4387MJ*MTY4MTYyNTQ5NS4yOS4xLjE2ODE2Mjg4NjMuNDYuMC4w' target='_blank'>
+              Помощь
+            </a>
+          </div>
+          <div>
+            <LanguageBtn />
+          </div>
         </div>
       </div>
     </div>
